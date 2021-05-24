912affed1ddf8568400ab2cf944c581101624612
Kanaan Ngutu <destinykaka@gmail.com>
2021-05-24 14:31:22 +1200

    added new subtheme

---
+++ docroot/themes/custom/covid19/js/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Behaviors for the covid19 theme.
+ */
+
+(function($, _, Drupal) {
+  Drupal.behaviors.covid19 = {
+    attach: function() {
+      // covid19 JavaScript behaviors goes here.
+    }
+  };
+})(window.jQuery, window._, window.Drupal);
